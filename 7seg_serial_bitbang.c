9c160db2167fa61894bd4ed66e87bf1c1f2400c5
BuildTools <unconfigured@null.spigotmc.org>
2016-10-09 12:10:20 +0200

    Added 7seg bitbang

---
+++ 7seg_serial_bitbang.c
@@ -0,0 +1,35 @@
+//Pin connected to ST_CP of 74HC595
+int latchPin = 8;
+//Pin connected to SH_CP of 74HC595
+int clockPin = 12;
+////Pin connected to DS of 74HC595
+int dataPin = 11;
+
+void setup() {
+  //set pins to output so you can control the shift register
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+const byte digits[10] = { 126, 72, 61, 109, 75, 103, 119, 76, 127, 111 };
+
+void loop() {
+  // count from 0 to 255 and display the number 
+  // on the LEDs
+  for (int numberToDisplay = 0; numberToDisplay <= 9; numberToDisplay++) {
+    // take the latchPin low so 
+    // the LEDs don't change while you're sending in bits:
+    digitalWrite(latchPin, LOW);
+    // shift out the bits:
+    shiftOut(dataPin, clockPin, MSBFIRST, digits[numberToDisplay]);  
+
+    //take the latch pin high so the LEDs will light up:
+    digitalWrite(latchPin, HIGH);
+    // pause before next value:
+
+    Serial.println(numberToDisplay);
+    delay(500);
+  }
+}
