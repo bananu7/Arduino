88fafb69fcb50524e4b20426499c7714295c50ec
bananu7 <bananu7@o2.pl>
2020-02-23 11:57:20 +0100

    Added the bme driver and improved serial command parsing

--- proj/weather_station/src/src/main.cpp
+++ proj/weather_station/src/src/main.cpp
@@ -1,57 +1,103 @@
 #include <Arduino.h>
 #include <SPI.h>
+
+#include <Bme280BoschWrapper.h>
+
 #include "winbond_spiflash.hpp"
- 
-/*
- */
-void loop(void) {
-  if (g_command_ready) {
-    if (g_command == "chipCmdIda") {
-      chipCmdIda();
-    }
-    else if (g_command == "chip_erase") {
-      chip_erase();
-    }
-    else if (g_command == "read_all_pages") {
-      read_all_pages();
-    }
-    // A one-parameter command...
-    else if (g_command.startsWith("read_page")) {
-      int pos = g_command.indexOf(" ");
-      if (pos == -1) {
-        Serial.println("Error: Command 'read_page' expects an int operand");
-      } else {
-        word page = (word)g_command.substring(pos).toInt();
-        read_page(page);
-      }
-    }
-    // A three-parameter command..
-    else if (g_command.startsWith("write_byte")) {
-      word pageno;
-      byte offset;
-      byte data;
-      
-      String args[3];
-      for (int i = 0; i < 3; ++i) {
-        int pos = g_command.indexOf(" ");
-        if (pos == -1) {
-          Serial.println("Syntax error in write_byte");
-          goto done;
+
+WinbondSpiFlash flash;
+
+bool g_command_ready(false);
+String g_command;
+
+void setup(void) {
+    SPI.begin();
+    SPI.setDataMode(0);
+    SPI.setBitOrder(MSBFIRST);
+    Serial.begin(9600);
+    Serial.println("");
+    //resetStatusReg();
+
+    Serial.println("Ready"); 
+}
+
+void serialEvent() {
+    char c;
+    while (Serial.available()) {
+        c = (char)Serial.read();
+        if (c == ';') {
+            g_command_ready = true;
         }
-        args[i] = g_command.substring(pos + 1);
-        g_command = args[i];
-      }
-      pageno = (word)args[0].toInt();
-      offset = (byte)args[1].toInt();
-      data = (byte)args[2].toInt();
-      write_byte(pageno, offset, data);
+        else {
+            g_command += c;
+        }    
     }
-    else {
-      Serial.print("Invalid command sent: ");
-      Serial.println(g_command);
+}
+
+template<int n>
+struct Result {
+    bool result;
+    int32_t data[n];
+};
+
+template<int n>
+Result<n> extractArguments(String const& str) {
+    int pos = 0;
+    Result<n> result;
+    result.result = false;
+
+    for (int i = 0; i < n; ++i) {
+        pos = str.indexOf(" ", pos);
+        if (pos == -1) {
+            Serial.println("Syntax error in write_byte");
+            return result;
+        }
+        // pos is pointing to space now
+        result.data[i] = str.substring(pos + 1).toInt();
+        // move pos to point to first digit
+        pos += 1;
     }
-done:
-    g_command = "";
-    g_command_ready = false;
-  }
+
+    result.result = true;
+    return result;
+}
+
+void loop(void) {
+    if (g_command_ready) {
+        if (g_command == "chipCmdIda") {
+            flash.chipCmdId();
+        }
+        else if (g_command == "chip_erase") {
+            flash.chip_erase();
+        }
+        else if (g_command == "read_all_pages") {
+            flash.read_all_pages();
+        }
+        else if (g_command.startsWith("read_page")) {
+            auto args = extractArguments<1>(g_command);
+            if (args.result == false) {
+                Serial.println("Error: Command 'read_page' expects an int operand");
+            } else {
+                auto const& page = args.data[0];
+                flash.read_page(page);
+            }
+        }
+        else if (g_command.startsWith("write_byte")) {
+            auto args = extractArguments<3>(g_command);
+            if (args.result == true) {
+                word pageno = args.data[0];
+                byte offset = args.data[1];
+                byte data = args.data[2];
+
+                flash.write_byte(pageno, offset, data);
+            }
+        }
+        else {
+            Serial.print("Invalid command sent: ");
+            Serial.println(g_command);
+        }
+
+        g_command = "";
+        g_command_ready = false;
+    }
 }
--- proj/weather_station/src/src/winbond_spiflash.hpp
+++ proj/weather_station/src/src/winbond_spiflash.hpp
@@ -1,3 +1,4 @@
+#pragma once
 
 #define writeEnable       0x06 // Address Write Enable
 #define writeDisable      0x04 // Address Write Disable
@@ -11,226 +12,183 @@
  
 const int WB_READ_STATUS_REG_1 = 0x05;
  
-bool g_command_ready(false);
 
-String g_command;
- 
-/*
-print_page_bytes() is a simple helper function that formats 256 bytes
- */
-void print_page_bytes(uint8_t *page_buffer) {
-  char buf[10];
-  for (int i = 0; i < 16; ++i) {
-    for (int j = 0; j < 16; ++j) {
-      sprintf(buf, "%02x", page_buffer[i * 16 + j]);
-      Serial.print(buf);
+class WinbondSpiFlash {
+    const int LED_PIN = 2;
+    /*
+    * See the timing diagram in section 9.2.10 of the
+    * data sheet located below, "Read Data (03h)".
+    */
+    void _read_page(word page_number, byte *page_buffer) {
+        digitalWrite(SS, HIGH);
+        digitalWrite(SS, LOW);
+        SPI.transfer(readData);
+        
+        // Construct the 24-bit address from the 16-bit page
+        // number and 0x00, since we will read 256 bytes (one
+        // page).
+        SPI.transfer((page_number >> 8) & 0xFF);
+        SPI.transfer((page_number >> 0) & 0xFF);
+        SPI.transfer(0);
+        for (int i = 0; i < 256; ++i) {
+            page_buffer[i] = SPI.transfer(0);
+        }
+        digitalWrite(SS, HIGH);
+        not_busy();
     }
-    Serial.println();
-  }
-}
- 
-/* 
- * See section 9.2.8 of the datasheet
- */
-const int LED_PIN = 2;
- 
-void not_busy(void) {
-  digitalWrite(LED_PIN, HIGH); 
-  digitalWrite(SS, HIGH);  
-  digitalWrite(SS, LOW);
-  SPI.transfer(WB_READ_STATUS_REG_1);       
-  while (SPI.transfer(0) & 1) {}; 
-  digitalWrite(SS, HIGH);
-  digitalWrite(LED_PIN, LOW);  
-}
 
-/*
- 
-This functions map to user commands. wrap the low-level calls with 
-print/debug statements to read
-*/
- 
-void chipCmdId(byte *b1, byte *b2, byte *b3) {
-  digitalWrite(SS, HIGH);
-  digitalWrite(SS, LOW);
-  SPI.transfer(chipCommandId);
-  *b1 = SPI.transfer(0); // manufacturer id
-  *b2 = SPI.transfer(0); // memory type
-  *b3 = SPI.transfer(0); // capacity
-  digitalWrite(SS, HIGH);
-  not_busy();
-}  
-
-/* 
- The chip command id  is fairly generic, just to verify function setup 
- */
-void chipCmdIda(void) {
-  Serial.println("Set Command: chipCmdIda");
-  byte b1, b2, b3;
-  chipCmdId(&b1, &b2, &b3);
-  char buf[128];
-  sprintf(buf, "ID: %02xh\nMemory Type: %02xh\nCapacity: %02xh",
-    b1, b2, b3);
-  Serial.println(buf);
-  Serial.println("Ready");
-} 
- 
- /*
- See the timing diagram in section 9.2.26 of the data sheet, "Chip Erase (C7h / 06h)". (Note:
- */
-void _chip_erase(void) {
-  digitalWrite(SS, HIGH);
-  digitalWrite(SS, LOW);  
-  SPI.transfer(writeEnable);
-  digitalWrite(SS, HIGH);
-  digitalWrite(SS, LOW);  
-  SPI.transfer(chipErase);
-  digitalWrite(SS, HIGH);
- 
-  /* See notes on rev 2 
-  digitalWrite(SS, LOW);  
-  SPI.transfer(writeDisable);
-  digitalWrite(SS, HIGH);
-  */
-  not_busy();
-}
-
-void chip_erase(void) {
-  Serial.println("command: chip_erase");
-  _chip_erase();
-  Serial.println("Ready");
-}
-
-/*
- * See the timing diagram in section 9.2.10 of the
- * data sheet located below, "Read Data (03h)".
- */
-void _read_page(word page_number, byte *page_buffer) {
-  digitalWrite(SS, HIGH);
-  digitalWrite(SS, LOW);
-  SPI.transfer(readData);
- 
-  // Construct the 24-bit address from the 16-bit page
-  // number and 0x00, since we will read 256 bytes (one
-  // page).
-  SPI.transfer((page_number >> 8) & 0xFF);
-  SPI.transfer((page_number >> 0) & 0xFF);
-  SPI.transfer(0);
-  for (int i = 0; i < 256; ++i) {
-    page_buffer[i] = SPI.transfer(0);
-  }
-  digitalWrite(SS, HIGH);
-  not_busy();
-}
- 
-
-void read_page(unsigned int page_number) {
-  char buf[80];
-  sprintf(buf, "command: read_page(%04xh)", page_number);
-  Serial.println(buf);
-  byte page_buffer[256];
-  _read_page(page_number, page_buffer);
-  print_page_bytes(page_buffer);
-  Serial.println("Ready");
-}
- 
-void read_all_pages(void) {
-  Serial.println("command: read_all_pages");
-  byte page_buffer[256];
-  for (int i = 0; i < 4096; ++i) {
-    _read_page(i, page_buffer);
-    print_page_bytes(page_buffer);
-  }
-  Serial.println("Ready");
-}
-
-/*
- * See the timing diagram in section 9.2.21 of the
- * data sheet, "Page Program (02h)".
- */
-void _write_page(word page_number, byte *page_buffer) {
-  digitalWrite(SS, HIGH);
-  digitalWrite(SS, LOW);  
-  SPI.transfer(writeEnable);
-  digitalWrite(SS, HIGH);
-  digitalWrite(SS, LOW);  
-  SPI.transfer(pageProgramStat);
-  SPI.transfer((page_number >>  8) & 0xFF);
-  SPI.transfer((page_number >>  0) & 0xFF);
-  SPI.transfer(0);
-  for (int i = 0; i < 256; ++i) {
-    SPI.transfer(page_buffer[i]);
-  }
-  digitalWrite(SS, HIGH);
-  ///* See notes on rev 2
-  digitalWrite(SS, LOW);  
-  SPI.transfer(writeDisable);
-  digitalWrite(SS, HIGH);
-  //*/
-  not_busy();
-} 
-
-void write_byte(word page, byte offset, byte databyte) {
-  char buf[80];
-  sprintf(buf, "command: write_byte(%04xh, %04xh, %02xh)", page, offset, databyte);
-  Serial.println(buf);
-  byte page_data[256];
-  _read_page(page, page_data);
-  page_data[offset] = databyte;
-  _write_page(page, page_data);
-  Serial.println("Ready");
-}
- 
-/*
- * string, setting a boolean used by the loop() routine
- * as a dispatch trigger.
- */
-void serialEvent() {
-  char c;
-  while (Serial.available()) {
-    c = (char)Serial.read();
-    if (c == ';') {
-      g_command_ready = true;
+    /*
+    See the timing diagram in section 9.2.26 of the data sheet, "Chip Erase (C7h / 06h)". (Note:
+    */
+    void _chip_erase(void) {
+        digitalWrite(SS, HIGH);
+        digitalWrite(SS, LOW);  
+        SPI.transfer(writeEnable);
+        digitalWrite(SS, HIGH);
+        digitalWrite(SS, LOW);  
+        SPI.transfer(chipErase);
+        digitalWrite(SS, HIGH);
+        
+        /* See notes on rev 2 
+        digitalWrite(SS, LOW);  
+        SPI.transfer(writeDisable);
+        digitalWrite(SS, HIGH);
+        */
+        not_busy();
     }
-    else {
-      g_command += c;
+
+    // Check if the memory is busy
+    void not_busy(void) {
+        digitalWrite(LED_PIN, HIGH); 
+        digitalWrite(SS, HIGH);  
+        digitalWrite(SS, LOW);
+        SPI.transfer(WB_READ_STATUS_REG_1);       
+        while (SPI.transfer(0) & 1) {}; 
+        digitalWrite(SS, HIGH);
+        digitalWrite(LED_PIN, LOW);  
+    }
+
+    void _chipCmdId(byte *b1, byte *b2, byte *b3) {
+        digitalWrite(SS, HIGH);
+        digitalWrite(SS, LOW);
+        SPI.transfer(chipCommandId);
+        *b1 = SPI.transfer(0); // manufacturer id
+        *b2 = SPI.transfer(0); // memory type
+        *b3 = SPI.transfer(0); // capacity
+        digitalWrite(SS, HIGH);
+        not_busy();
+    }  
+
+    /*
+    print_page_bytes() is a simple helper function that formats 256 bytes
+    */
+    void print_page_bytes(uint8_t *page_buffer) {
+        char buf[10];
+        for (int i = 0; i < 16; ++i) {
+            for (int j = 0; j < 16; ++j) {
+            sprintf(buf, "%02x", page_buffer[i * 16 + j]);
+            Serial.print(buf);
+            }
+            Serial.println();
+        }
+    }
+
+    void error(uint32_t d) {
+        pinMode(LED_PIN, OUTPUT);
+        for(;;) {
+            digitalWrite(LED_PIN, HIGH);
+            delay(d);
+            digitalWrite(LED_PIN, LOW);
+            delay(d);
+        }
+    }
+
+    void resetStatusReg() {
+        Serial.println("Reset status reg"); 
+        digitalWrite(SS, HIGH);
+        digitalWrite(SS, LOW);  
+        SPI.transfer(writeEnable);
+        digitalWrite(SS, HIGH);
+        digitalWrite(SS, LOW);
+        SPI.transfer(0x01);
+        SPI.transfer(0);
+        SPI.transfer(0);
+        digitalWrite(SS, HIGH);
+        not_busy();
+    }  
+
+public:
+    void chipCmdId(void) {
+        Serial.println("Set Command: chipCmdIda");
+        byte b1, b2, b3;
+        _chipCmdId(&b1, &b2, &b3);
+        char buf[128];
+        sprintf(buf, "ID: %02xh\nMemory Type: %02xh\nCapacity: %02xh",
+            b1, b2, b3);
+        Serial.println(buf);
+        Serial.println("Ready");
+    } 
+
+    void chip_erase(void) {
+        Serial.println("command: chip_erase");
+        _chip_erase();
+        Serial.println("Ready");
     }    
-  }
-}
 
-void error(uint32_t d) {
-  for(;;) {
-    digitalWrite(LED_PIN, HIGH);
-    delay(d);
-    digitalWrite(LED_PIN, LOW);
-    delay(d);
-  }
-}
+    void read_page(unsigned int page_number) {
+        char buf[80];
+        sprintf(buf, "command: read_page(%04xh)", page_number);
+        Serial.println(buf);
+        byte page_buffer[256];
+        _read_page(page_number, page_buffer);
+        print_page_bytes(page_buffer);
+        Serial.println("Ready");
+    }
+    
+    void read_all_pages(void) {
+        Serial.println("command: read_all_pages");
+        byte page_buffer[256];
+        for (int i = 0; i < 4096; ++i) {
+            _read_page(i, page_buffer);
+            print_page_bytes(page_buffer);
+        }
+        Serial.println("Ready");
+    }
 
-void resetStatusReg() {
-  Serial.println("Reset status reg"); 
-  digitalWrite(SS, HIGH);
-  digitalWrite(SS, LOW);  
-  SPI.transfer(writeEnable);
-  digitalWrite(SS, HIGH);
-  digitalWrite(SS, LOW);
-  SPI.transfer(0x01);
-  SPI.transfer(0);
-  SPI.transfer(0);
-  digitalWrite(SS, HIGH);
-  not_busy();
-}  
- 
-void setup(void) {
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
-  
-  SPI.begin();
-  SPI.setDataMode(0);
-  SPI.setBitOrder(MSBFIRST);
-  Serial.begin(9600);
-  Serial.println("");
-  //resetStatusReg();
-  
-  Serial.println("Ready"); 
-}
+    void write_byte(word page, byte offset, byte databyte) {
+        char buf[80];
+        sprintf(buf, "command: write_byte(%04xh, %04xh, %02xh)", page, offset, databyte);
+        Serial.println(buf);
+        byte page_data[256];
+        _read_page(page, page_data);
+        page_data[offset] = databyte;
+        write_page(page, page_data);
+        Serial.println("Ready");
+    }
+
+    void write_page(word page_number, byte *page_buffer) {
+        /*
+        * See the timing diagram in section 9.2.21 of the
+        * data sheet, "Page Program (02h)".
+        */
+        digitalWrite(SS, HIGH);
+        digitalWrite(SS, LOW);  
+        SPI.transfer(writeEnable);
+        digitalWrite(SS, HIGH);
+        digitalWrite(SS, LOW);  
+        SPI.transfer(pageProgramStat);
+        SPI.transfer((page_number >>  8) & 0xFF);
+        SPI.transfer((page_number >>  0) & 0xFF);
+        SPI.transfer(0);
+        for (int i = 0; i < 256; ++i) {
+            SPI.transfer(page_buffer[i]);
+        }
+        digitalWrite(SS, HIGH);
+        ///* See notes on rev 2
+        digitalWrite(SS, LOW);  
+        SPI.transfer(writeDisable);
+        digitalWrite(SS, HIGH);
+        //*/
+        not_busy();
+    } 
+};
