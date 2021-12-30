af9b36b89b0594de120960fa22413e5bd52b57ab
bananu7 <bananu7@o2.pl>
2021-12-30 18:29:23 +0100

    Removed ansible stuff, replaced with a simple deploy script

---
+++ proj/spotibox_pi/deploy.sh
@@ -0,0 +1,8 @@
+USER=pi
+HOST=192.168.1.114
+REMOTE=${USER}@${HOST}
+
+rsync -a ./flask/ ${REMOTE}:/var/www/flask
+rsync www/uwsgi.ini ${REMOTE}:/etc/uwsgi/apps-enabled/text.ini
+rsync www/nginx-site ${REMOTE}:/etc/nginx/sites-available/default
+
--- proj/spotibox_pi/flask/app.py
+++ proj/spotibox_pi/flask/app.py
@@ -13,7 +13,9 @@ def ix():
 def text():
     t = request.args.get('t', default='', type=str)
     lcd_init()
-    lcd_string(t, LCD_LINE_1)
+    lcd_string(t[0:20], LCD_LINE_1)
+    lcd_string(t[20:40], LCD_LINE_2)
+    lcd_string(t[40:60], LCD_LINE_3)
     return "ok"
 
 if __name__== '__main__':
